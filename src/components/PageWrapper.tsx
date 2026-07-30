7dea31d0bd1910de4d3807f86df04ed9a80133d4
anandksri <anandkeshari0711@gmail.com>
2026-07-30 11:11:36 +0545

    fixing page wrappere

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,30 +1,22 @@
 "use client";
 
 import { motion } from "motion/react";
-
 import { ReactNode } from "react";
 
-export default function PageWrapper({ children }: { children: ReactNode }) {
+export default function PageWrapper({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <motion.main
-      initial={{
-        opacity: 0,
-        y: 24,
-        filter: "blur(6px)",
-        scale: 0.99,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-        filter: "blur(0px)",
-        scale: 1,
-      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{
-        duration: 0.45,
-        ease: [0.22, 1, 0.36, 1],
+        duration: 0.25,
       }}
     >
       {children}
     </motion.main>
   );
-}
+} 
